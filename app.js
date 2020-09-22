0d0db38be0bc95e868af3d815e6d74273455164b
Nada Slama <ns336668@gmail.com>
2020-09-22 15:06:04 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-swal("اضحك من قلبك", {
-    buttons: false,
-    timer: 2000,
-  });
+
 const button =document.getElementById("butt");
+const cont=document.getElementById("container")
 button.addEventListener("click", addJoke);
+
+
 function addJoke(){
+    console.log("the button is clicked")
     let xhr= new XMLHttpRequest()
     console.log(xhr)
-    xhr.open('GET','index.json',true)
-    xhr.onload=function(){
-        if (xhr.status===200){
-            console.log("this request is status"+xhr.status)
-            const cont=document.getElementById("container")
-            // cont.innerHTML=this.responseText
-            let joke=JSON.parse(this.responseText)
-            joke.forEach(joke =>{
-                console.log(joke)
-             let newDiv=document.createElement("div")
-             let jokeCont=`<span id=num>${joke.id}</span><h3 id=title>${joke.title}</h3><h4 id=text>${joke.text}</h4> `
-            console.log(jokeCont)
-            newDiv.innerHTML=jokeCont
-            cont.appendChild(newDiv)
-            
-         } 
+    xhr.open('GET','index.json')
+    xhr.onload= function(){
 
-         )
+        if (xhr.status === 200){
+            console.log("this request is status" + xhr.status)
+          
+            let joke=JSON.parse(this.responseText)
+            
+            joke.filter(joke =>{
+                
+                if (joke.id === getRndInteger(1, 52)){
+                    let jokee = joke;
+                    console.log(jokee)
+                    let jokeCont=document.createElement("div");
+                   jokeCont.innerHTML=`<h4 id="query">${joke.title}</h4><h4 id="answer">${joke.text}</h4>`
+                   cont.appendChild(jokeCont)
+                }else {
+                    console.log("no jokes for you today")
+                }
+                
+                
+         })
              
          
-        //  console.log(joke)
+        
          
 
         }
@@ -36,3 +41,8 @@ function addJoke(){
     
     xhr.send();
 }
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
